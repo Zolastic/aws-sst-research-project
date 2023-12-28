307d7de7cdd69bd06c1d834abbea2d3c0662ff4e
Zolastic <honhattienlul@gmail.com>
2023-12-28 17:00:57 +0800

    feat: finished add function for to-do list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,53 @@
 import { api } from "@/trpc/server";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import CreatePostIt from "@/components/createPostIt";
+
 export default async function Home() {
   const postIts = await api.postIt.getAll.query();
 
   return (
-    <main className="flex min-h-screen flex-col items-center bg-slate-50">
-      <h1 className="mb-4 mt-5 text-4xl font-bold">
-        Welcome to <span className="text-purple-400">SST</span> To-do List Add
-        Function Demo!
-      </h1>
+    <main className="flex min-h-screen flex-col bg-slate-50">
+      <div className="flex justify-center">
+        <h1 className="mb-4 mt-5 text-4xl font-bold">
+          Welcome to <span className="text-purple-400">SST</span> To-do List Add
+          Function Demo!
+        </h1>
+      </div>
+
+      <div className="mt-5 flex justify-center">
+        <CreatePostIt />
+      </div>
+
+      <div className="mt-5 flex justify-center">
+        {postIts.length > 0 ? (
+          <div className="flex w-[1000px] flex-wrap items-start p-[8px]">
+            {postIts.map((postIt) => (
+              <Card key={postIt.id} className="mx-2">
+                <CardHeader>
+                  <CardTitle>{postIt.name}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription>{postIt.content}</CardDescription>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-lg">
+              Looks like you don't have any to-do item.
+            </h1>
+            <h1 className="text-lg">Create your first one!</h1>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
---
+++ src/components/createPostIt.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useState } from "react";
+
+type Props = {};
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { api } from "@/trpc/react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+const CreatePostIt = (props: Props) => {
+  const [name, setName] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const router = useRouter();
+
+  const createPost = api.postIt.create.useMutation();
+
+  const addPostIt = async () => {
+    toast.promise(
+      createPost.mutateAsync({
+        name: name,
+        content: content,
+      }),
+      {
+        loading: "Adding...",
+        success: () => {
+          router.refresh();
+          return "Added!";
+        },
+        error: "Error!",
+      },
+    );
+  };
+
+  return (
+    <>
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Add Post-It!</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add Post-It!</DialogTitle>
+              <DialogDescription>
+                Simply give it a name and a content.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex items-center space-x-2">
+              <div className="grid flex-1 gap-2">
+                <Input
+                  id="name"
+                  placeholder="Name"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center space-x-2">
+              <div className="grid flex-1 gap-2">
+                <Textarea
+                  id="content"
+                  placeholder="Content"
+                  onChange={(e) => {
+                    setContent(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+
+            <DialogFooter className="sm:justify-start">
+              <DialogClose asChild>
+                <Button type="button" variant="secondary" onClick={addPostIt}>
+                  Add!
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
+  );
+};
+
+export default CreatePostIt;
